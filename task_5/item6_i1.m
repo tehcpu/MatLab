2fb0f59cbf7947ddbffc850a2ada300d37938db3
cupcoder <cupcoder@gmail.com>
2013-12-15 20:45:02 +0400

    0.3.3
    some fixes ..

--- task_5/item6_i1.m
+++ task_5/item6_i1.m
@@ -1,3 +1,4 @@
-function y=item6_i1(x)
-
+function o=item6_i1(x)
+    [r, c, v] = find(x==0);           % находим индексы строк с нулями (r)
+    o=size(x,1) - length(unique(r));  % вычитаем из кол - ва всех строк кол - во строк с нулями
 end
